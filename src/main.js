d54d51b925a83f54cf110a45459d9f7d54ac500f
Your Name <you@example.com>
2021-03-13 17:47:13 +0700

    cloud

--- src/main.js
+++ src/main.js
@@ -2,8 +2,29 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue } from 'bootstrap-vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBl7LoJnUfTxnXiNld4JaPiHLa2CeFUyw8",
+  authDomain: "fir-ada2e.firebaseapp.com",
+  databaseURL: "https://fir-ada2e-default-rtdb.firebaseio.com",
+  projectId: "fir-ada2e",
+  storageBucket: "fir-ada2e.appspot.com",
+  messagingSenderId: "877989171330",
+  appId: "1:877989171330:web:867b47e2e8c1c2f1e8649b",
+  measurementId: "G-D9P6BLVFBT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore()
 
 new Vue({
   router,
---
+++ src/views/firestore.vue
@@ -0,0 +1,137 @@
+<template>
+  <div  >
+    <h1 class="t">SIGN UP</h1>
+    <br>
+    <div class="container">
+     
+      <b-form> <br>
+        <b-form-group
+          id="input-group-2"
+          class="tt"
+          label="First Name"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="name"
+            type="text"
+            placeholder="ชื่อ "
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-2"
+          class="tt"
+          label="Last Name"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="lastname"
+            type="text"
+            placeholder="นามสกุล "
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          class="tt"
+          id="input-group-2"
+          label="Phone Number"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="phonenumber"
+            type="text"
+            placeholder="เบอร์โทรศัพท์ "
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          class="tt"
+          id="input-group-1"
+          label="Email Address"
+          label-for="input-1"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="email"
+            type="email"
+            placeholder="อีเมล "
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-2"
+          class="tt"
+          label="Password"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="password"
+            placeholder="รหัสผ่าน "
+            type="password"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button class="btn"  @click="onSubmit"
+          >Sign Up</b-button 
+        >
+      </b-form><br>
+      
+    </div>
+  </div>
+ 
+</template>
+
+<script>
+import { db } from "@/main.js";
+export default {
+  data() {
+    return {
+      Firstname: "",
+      Lastname: "",
+      phonenumber: "",
+      Email: "",
+      Password: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      var form = {
+        Firstname: this.name,
+        Lastname: this.lastname,
+        Phonenumber: this.phonenumber,
+        Email: this.email,
+        Password: this.password,
+      };
+      db.collection("firestore")
+        .doc()
+        .set(form);
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  background: rgba(221, 207, 255, 0.89);
+}
+.t{
+     background: rgba(247, 247, 247, 0.89);
+}
+.tt {
+  margin-left: 17px;
+  text-align: left;
+}
+.btn{
+    color: blueviolet;
+}
+
+</style>
